731e9e3275a760f4d2090442dd043475549c2740
Luke Patrick <luke@routerchowder.com>
2020-06-14 22:28:48 -0600

    fix comment typos in ubuntu-install

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,78 +1,78 @@
 #!/bin/bash
 
 # basic setup stuff
-## sudo apt update
+sudo apt update
 
 # get some basic packages
-## sudo apt install git \
-    ## curl
+sudo apt install git \
+    curl
 
 # generate an ssh key
-## ssh-keygen
+ssh-keygen
 
 # install zsh
-## sudo apt install zsh
+sudo apt install zsh
 # setup oh-my-zsh
-## sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # set default shell to zsh
-## chsh -s $(which zsh)
+chsh -s $(which zsh)
 # copy .zshrc to home
-## mv ~/.zshrc ~/.zshrc.oh-my-zsh
-## cp .zshrc ~/.zshrc
-## install zsh syntax highlighting
-## git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+mv ~/.zshrc ~/.zshrc.oh-my-zsh
+cp .zshrc ~/.zshrc
+install zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # nvm and node
-## curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
-## nvm install node
-## nvm use node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
+nvm install node
+nvm use node
 # setup yarn
-## curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-## echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-## sudo apt update && sudo apt install --no-install-recommends yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install --no-install-recommends yarn
 
 # pure prompt stuff
-## create directory for git clone
-## mkdir -p "$HOME/.zsh"
-## git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+create directory for git clone
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 
 # neovim and vim stuff
-## sudo apt install neovim
-## ln -sf "${PWD}"/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
-## ln -sf "${PWD}"/.config/nvim/init.vim ~/.config/nvim/init.vim
-## use :CocInstall coc-json coc-tsserver coc-rls
-## to complete Coc installation
+sudo apt install neovim
+ln -sf "${PWD}"/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sf "${PWD}"/.config/nvim/init.vim ~/.config/nvim/init.vim
+use :CocInstall coc-json coc-tsserver coc-rls
+to complete Coc installation
 
 # add Vim Plug
-## sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    ## https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-## vim +'PlugInstall --sync' +qa
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+vim +'PlugInstall --sync' +qa
 
 # add tmux
-## sudo apt install tmux
-## ln -sf "${PWD}"/.tmux.conf ~/.tmux.conf
+sudo apt install tmux
+ln -sf "${PWD}"/.tmux.conf ~/.tmux.conf
 # setup tpm
-## git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # setup rust
-## curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 
 # setup docker
-## sudo apt-get install \
-##     apt-transport-https \
-##     ca-certificates \
-##     gnupg-agent \
-##     software-properties-common
-## 
-## curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-## sudo apt-key fingerprint 0EBFCD88
-## sudo add-apt-repository \
-##    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-##    $(lsb_release -cs) \
-##    stable"
-## 
-## sudo apt-get update
-## sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 # add user to docker group
-## sudo groupadd docker
-## sudo usermod -aG docker $USER
+sudo groupadd docker
+sudo usermod -aG docker $USER
