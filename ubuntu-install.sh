f4f890a62d6d438e080bf06aa7806a8538c3cb8b
Luke Patrick <luke@routerchowder.com>
2020-06-16 22:37:21 -0600

    fix call to nvm that fails

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -18,6 +18,10 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # nvm and node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
+# load nvm so we can use it
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 nvm use node
 # setup yarn
