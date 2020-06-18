1582ef5dae65956a309db713e4550ead7bc82ad0
Luke Patrick <luke@routerchowder.com>
2020-06-17 23:56:05 -0600

    create an arch-setup script and rename arc-menu

---
+++ arch-install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# basic setup stuff
+sudo pacman -Syu
+
+# setup oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# bump out here and  re-run as we swap
+# to ohmyzsh as default terminal from prompt
+
+# copy .zshrc to home
+mv ~/.zshrc ~/.zshrc.arch-backup
+cp .zshrc ~/.zshrc
+
+# install zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# nvm and node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
+# load nvm so we can use it
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node
+nvm use node
+
+# setup yarn
+sudo pacman -S yarn
+
+# pure prompt stuff
+# create directory for git clone
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
+# neovim and vim stuff
+sudo pacman -S neovim ripgrep
+mkdir ~/.config/nvim
+ln -sf "${PWD}"/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sf "${PWD}"/.config/nvim/init.vim ~/.config/nvim/init.vim
+
+# use :CocInstall coc-json coc-tsserver coc-rls coc-eslint
+# to complete Coc installation
+
+# add Vim Plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+nvim +'PlugInstall --sync' +qa
+
+# add tmux
+sudo pacman -S tmux
+ln -sf "${PWD}"/.tmux.conf ~/.tmux.conf
+# setup tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# setup rust
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+
+# setup docker
+sudo pacman -S docker
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+
+# add user to docker group
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+
+
