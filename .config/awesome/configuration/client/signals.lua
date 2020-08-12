bde2a97dfc06b6b85737150ceca0735099f07104
Luke Patrick <luke@routerchowder.com>
2020-08-11 23:03:39 -0600

    refator(awesome): Nuke sloppy focus so I do not go insane

--- .config/awesome/configuration/client/signals.lua
+++ .config/awesome/configuration/client/signals.lua
@@ -22,31 +22,3 @@ client.connect_signal(
 		end
 	end
 )
-
--- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal(
-	'mouse::enter',
-	function(c)
-		c:emit_signal(
-			'request::activate',
-			'mouse_enter',
-			{
-				raise = true
-			}
-		)
-	end
-)
-
-client.connect_signal(
-	'focus',
-	function(c)
-		-- c.border_color = beautiful.border_focus
-	end
-)
-
-client.connect_signal(
-	'unfocus',
-	function(c)
-		-- c.border_color = beautiful.border_normal
-	end
-)
