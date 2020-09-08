61b86baee870cc801692f8b23561d1aa293699aa
Luke Patrick <luke@routerchowder.com>
2020-09-07 23:15:08 -0600

    feat(config): Add default chromium config for flags with package changes to awesome script

--- awesome-config.sh
+++ awesome-config.sh
@@ -4,7 +4,8 @@
 sudo pacman -S nitrogen redshift kitty \
   yay rofi inter-font noto-fonts-emoji \
   lxappearance-gtk3 flameshot playerctl \
-  arc-gtk-theme
+  arc-gtk-theme chromium pulseeffects \
+  tlpui tlp-rdw s-tui
 
 # AUR packages
 yay -S awesome-git picom-tryone-git \
@@ -15,6 +16,7 @@ ln -sf "${PWD}"/.config/redshift ~/.config/redshift
 ln -sf "${PWD}"/.config/kitty ~/.config/kitty
 ln -sf "${PWD}"/.config/awesome ~/.config/awesome
 ln -sf "${PWD}"/.config/nitrogen ~/.config/nitrogen
+ln -sf "${PWD}"/.config/chromium-flags.conf ~/.config/chromium-flags.conf
 
 # kick off redshift
 systemctl --user enable --now redshift.service
