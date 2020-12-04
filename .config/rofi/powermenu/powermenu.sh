f0e6422a362c8bc504acb01907ea2e72035dd68c
Luke Patrick <luke@routerchowder.com>
2020-12-03 21:56:24 -0700

    refactor(rofi): clean up rofi power script

--- .config/rofi/powermenu/powermenu.sh
+++ .config/rofi/powermenu/powermenu.sh
@@ -1,93 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-dir="$HOME/.config/rofi/powermenu"
-rofi_command="rofi -theme $dir/five.rasi"
+# small power menu using rofi, i3, systemd and pm-utils
+# (last 3 dependencies are adjustable below)
+# tostiheld, 2016
 
-uptime=$(uptime -p | sed -e 's/up //g')
+poweroff_command="systemctl poweroff"
+reboot_command="systemctl reboot"
+logout_command="i3-msg exit"
+hibernate_command="systemctl hibernate"
+suspend_command="systemctl suspend"
 
-# Options
-shutdown=""
-reboot=""
-lock=""
-suspend=""
-logout=""
+# you can customise the rofi command all you want ...
+rofi_command="rofi -width 10 -hide-scrollbar -bg #586e75 -opacity 100 -padding 5"
 
-# # Confirmation
-# confirm_exit() {
-# 	rofi -dmenu\
-# 		-i\
-# 		-no-fixed-num-lines\
-# 		-p "Are You Sure? : "\
-# 		-theme $dir/confirm.rasi
-# }
+options=$'poweroff\nreboot\nlogout\nhibernate\nsuspend' 
 
-# # Message
-# msg() {
-# 	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
-# }
-
-# Variable passed to rofi
-options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
-
-chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 2)"
-case $chosen in
-    $shutdown)
-		# ans=$(confirm_exit &)
-		ans="yes"
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl poweroff
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $reboot)
-		# ans=$(confirm_exit &)
-		ans="yes"
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl reboot
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $lock)
-		if [[ -f /usr/bin/betterlockscreen ]]; then
-			betterlockscreen -l
-		elif [[ -f /usr/bin/i3lock ]]; then
-			i3lock
-		fi
-        ;;
-    $suspend)
-		# ans=$(confirm_exit &)
-		ans="yes"
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $logout)
-		# ans=$(confirm_exit &)
-		ans="yes"
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-				i3-msg exit
-			fi
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-esac
+# ... because the essential options (-dmenu and -p) are added here
+eval \$"$(echo "$options" | $rofi_command -dmenu -p "")_command"
