398c0eb9ae9d24d7a5e16a2f49b31fdce151ae57
Luke Patrick <luke@routerchowder.com>
2020-12-07 16:47:13 -0700

    refactor(rofi): nuke hibernate option from power menu

--- .config/rofi/powermenu/powermenu.sh
+++ .config/rofi/powermenu/powermenu.sh
@@ -7,13 +7,13 @@
 poweroff_command="systemctl poweroff"
 reboot_command="systemctl reboot"
 logout_command="i3-msg exit"
-hibernate_command="systemctl hibernate"
+# hibernate_command="systemctl hibernate"
 suspend_command="systemctl suspend"
 
 # you can customise the rofi command all you want ...
 rofi_command="rofi -width 10 -hide-scrollbar -bg #586e75 -opacity 100 -padding 5"
 
-options=$'poweroff\nreboot\nlogout\nhibernate\nsuspend' 
+options=$'poweroff\nreboot\nlogout\nsuspend' 
 
 # ... because the essential options (-dmenu and -p) are added here
 eval \$"$(echo "$options" | $rofi_command -dmenu -p "")_command"
