1a328727460adb8f89f14bb30c89acccb90f0867
Luke Patrick <luke@routerchowder.com>
2022-07-02 10:30:45 -0600

    refactor: rework polybar to be a hell of a lot snazzier

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar -r example >>/tmp/polybar1.log 2>&1 & disown
-done
-
-echo "Bars launched..."
+polybar main -c ~/.config/polybar/config.ini &
